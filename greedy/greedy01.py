25a6ff299e5fbe29596fd862b1f5233bacc96be7
ujin <dklvmf0106@gmail.com>
2022-01-21 20:32:42 +0900

    no message

---
+++ greedy/greedy01.py
@@ -0,0 +1,17 @@
+# 1이 될 때 까지
+
+n, k = map(int, input().split())
+count = 0
+
+while True:
+    target = (n//k)*k
+    count += (n-target)
+    n = target
+    if n < k :
+        break
+
+    count += 1
+    n //= k
+
+count += (n-1)
+print(count)
