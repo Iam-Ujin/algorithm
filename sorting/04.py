a00d28e2170b116f07e12646714990bb23b4615d
ujin <dklvmf0106@gmail.com>
2022-01-24 15:45:14 +0900

    계수정렬

---
+++ sorting/04.py
@@ -0,0 +1,15 @@
+# 계수 정렬
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+print(sorted(array))
+print('------------------------------')
+
+# 모든 범위를 포함하는 리스트 선언 ( 모든 값을 0으로 초기화 )
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=' ')
